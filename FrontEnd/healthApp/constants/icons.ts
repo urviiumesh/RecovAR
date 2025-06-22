73d29d6a44de948459bd94f5857d16c5c6f78dd6
Nithish Reddy <nithishreddyb2@gmail.com>
2025-06-22 22:30:49 +0530

    added files

---
+++ FrontEnd/healthApp/constants/icons.ts
@@ -0,0 +1,27 @@
+import home from "@/assets/icons/home.png";
+import search from "@/assets/icons/search.png";
+import person from "@/assets/icons/person.png";
+import logo from "@/assets/icons/logo.png";
+import save from "@/assets/icons/save.png";
+import star from "@/assets/icons/star.png";
+import play from "@/assets/icons/play.png";
+import arrow from "@/assets/icons/arrow.png";
+import exercise from "@/assets/icons/exercise.png";
+import reminder from "@/assets/icons/reminder.png";
+import Utilities from "@/assets/icons/Utilities.png";
+import emergency from "@/assets/icons/Emergency.png";
+
+export const icons = {
+  home,
+  search,
+  person,
+  logo,
+  save,
+  star,
+  play,
+  arrow,
+  exercise,
+  reminder,
+  Utilities,
+  emergency,
+};
